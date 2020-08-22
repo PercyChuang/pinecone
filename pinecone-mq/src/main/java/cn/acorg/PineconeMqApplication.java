cbe8a21a87a5a01dd770865ffe07fb773e2e39f7
ChenXueSong <1216926289@qq.com>
2020-08-22 21:30:35 +0800

    // 消息中心初始化

---
+++ pinecone-mq/src/main/java/cn/acorg/PineconeMqApplication.java
@@ -0,0 +1,17 @@
+package cn.acorg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 消息中心
+ * @author ChenXueSong
+ */
+@SpringBootApplication
+public class PineconeMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PineconeMqApplication.class, args);
+    }
+
+}
