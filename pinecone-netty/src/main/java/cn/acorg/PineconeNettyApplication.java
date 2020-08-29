8d546a6b28feacb2ee102fa540b0a357f49cba8b
ChenXueSong <1216926289@qq.com>
2020-08-29 16:28:28 +0800

    // mq服务修改配置，netty 服务建立

---
+++ pinecone-netty/src/main/java/cn/acorg/PineconeNettyApplication.java
@@ -0,0 +1,13 @@
+package cn.acorg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PineconeNettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PineconeNettyApplication.class, args);
+    }
+
+}
---
+++ pinecone-netty/src/test/java/cn/acorg/PineconeNettyApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.acorg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PineconeNettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
